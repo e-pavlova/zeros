b02054378512142477b0f111d8b6456216c9c06e
Ekaterina Pavlova <pavlova.ea.91@gmail.com>
2018-02-25 00:48:11 +0300

    Implement function which counts the number of zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports = function getZerosCount(number)
+{
+    let countOfZeros = 0;
+    let i = 5;
+
+    while ((number / i) >= 1)
+    {
+        countOfZeros += Math.floor(number / i);
+        i *= 5;
+    }
+    return countOfZeros;
 }
